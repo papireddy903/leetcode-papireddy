d41b6af9bbc0261834e2f0c42baf33217d2f3c9c
papireddy903 <97383201+papireddy903@users.noreply.github.com>
2023-10-17 11:04:16 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (7.07%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,22 +1,21 @@
 class Solution {
     private:
-    int helper(int pivot, int j,vector<vector<int>>&dp){
+    int helper(int pivot,int j, vector<vector<int>> &dp){
         if (pivot >= j) return 1;
-        if (dp[pivot][j] != -1) return dp[pivot][j];
-        int res=0;
+        if (pivot )
+        if (dp[pivot][j]!=-1) return dp[pivot][j];
+        int ans = 0;
         for (int k = pivot;k<=j;k++){
-           res += helper(pivot,k-1,dp) * helper(k+1,j,dp);
+            ans += helper(pivot,k-1,dp) * helper(k+1,j,dp);
         }
-
-        return dp[pivot][j] = res;
-        
-
+        return dp[pivot][j] = ans;
     }
 public:
     int numTrees(int n) {
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+
+        vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
         int pivot = 1;
-        return helper(1,n,dp);
+        return helper(pivot,n,dp);
         
     }
 };
