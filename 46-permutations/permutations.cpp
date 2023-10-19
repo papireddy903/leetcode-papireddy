6b9588fb2283df6dd3dc6c3a35527f9540a0e892
papireddy903 <97383201+papireddy903@users.noreply.github.com>
2023-10-19 11:07:07 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (45.05%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,31 +1,32 @@
 class Solution {
-private:
-    void helper(int ind,vector<int> &nums, vector<vector<int>> &ans) {
-        if (ind == nums.size()){
-            ans.push_back(nums);
-            return ;
-        }
-        
-        for (int i = ind; i<nums.size(); i++){
-                swap(nums[ind],nums[i]);
-                helper(ind+1,nums,ans);
-                swap(nums[ind],nums[i]);
+    private:
+        void permuteArray(vector<int> &nums,vector<vector<int>> &ans,vector<int>&ds,vector<int> &freq){
+            if (ds.size() == nums.size()){
+                ans.push_back(ds);
+                return ;
+            }
+
+            for (int i = 0;i<nums.size();i++){
+                if (!freq[i]){
+                    freq[i]++;
+                    ds.push_back(nums[i]);
+                    permuteArray(nums,ans,ds,freq);
+                    freq[i]--;
+                    ds.pop_back();
+
+                }
+            }
             
         }
-        
 
-        
-    }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
-        // vector<int> ds;
-        int ind = 0;
         int n = nums.size();
-        // vector<int> visited(n,0);
-        
-        helper(0,nums, ans);
-        
+        vector<vector<int>> ans;
+        vector<int> ds;
+        vector<int> freq(n,0);
+        permuteArray(nums,ans,ds,freq);
         return ans;
+        
     }
 };
