396cd118ac559287dad4454095748be292dbce82
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-04-14 01:05:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (6.29%) - LeetSync

---
+++ 899-binary-gap/binary-gap.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int maxi = 0;
+        int dist = 0;
+        string dig = "";
+        while (n){
+            dig += to_string(n%2);
+            n = n/2;
+        }
+
+        reverse(dig.begin(),dig.end());
+        int ind;
+        for (int i = 0;i<dig.size();i++){
+            if (dig[i] == '1'){
+                ind = i;
+                break;
+            }
+        }
+        cout << "first one : " << ind << endl;
+        int count = 1;
+
+        for (int i = ind+1;i<dig.size();i++){
+            
+            if (dig[i]=='1'){
+                dist = i - ind;
+                cout << "dist: " << dist << endl;
+                maxi = max(dist,maxi);
+                ind = i;
+            }
+        }
+
+        return maxi;
+
+
+
+        
+    }
+};
