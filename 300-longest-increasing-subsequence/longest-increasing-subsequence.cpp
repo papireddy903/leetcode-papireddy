2723c02bca343159601e4c4983083b9b95c15a6d
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-05-06 22:31:49 +0530

    Time: 4 ms (94.10%) | Memory: 12.8 MB (80.67%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,3 +1,4 @@
+#include<bits/stdc++.h>
 class Solution {
     private:
     int helper(int ind, int prev,vector<int> &nums, vector<vector<int>> &dp){
@@ -16,21 +17,35 @@ class Solution {
     }
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        // vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
-        vector<int> dp(n,1);
-        int maxi = 1;
-        for (int i = 0;i<n;i++){
-            for (int j = 0;j<i;j++){
-                if (nums[j] < nums[i]){
-                    dp[i] = max(dp[i], 1+dp[j]);
-                }
-            }
-            maxi = max(maxi, dp[i]);
-        }
-        return maxi;
+        // int n = nums.size();
+        // // vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+        // vector<int> dp(n,1);
+        // int maxi = 1;
+        // for (int i = 0;i<n;i++){
+        //     for (int j = 0;j<i;j++){
+        //         if (nums[j] < nums[i]){
+        //             dp[i] = max(dp[i], 1+dp[j]);
+        //         }
+        //     }
+        //     maxi = max(maxi, dp[i]);
+        // }
+        // return maxi;
 
         // return helper(0,-1,nums,dp);
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        int len = 1;
+        for (int i = 1;i<nums.size();i++){
+            if (nums[i] > ans.back()){
+                ans.push_back(nums[i]);
+                len++;
+            }
+            else{
+                int ind = lower_bound(ans.begin(),ans.end(), nums[i])-ans.begin();
+                ans[ind] = nums[i];
+            }
+        }
+        return len;
         
     }
 };
