348ddce56b8d25491b32a03b50f6dd6a45a8f1c0
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-05-08 23:58:38 +0530

    Time: 14 ms (11.65%) | Memory: 8.6 MB (60.69%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // sol 1
-        sort(s.begin(), s.end());
+        
+        sort(s.begin(),s.end());
         sort(t.begin(), t.end());
 
-        return s==t;
-
+        if (s==t){
+            return true;
+        }
+        else{
+            return false;
+        }
         
     }
 };
