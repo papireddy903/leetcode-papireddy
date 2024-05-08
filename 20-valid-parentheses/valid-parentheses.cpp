f45a889cc73e1aae4b23800c6fc4ba1a70199d2f
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-05-09 01:01:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (59.57%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,28 +1,27 @@
+#include<stack>
 class Solution {
+    
 public:
     bool isValid(string s) {
-        stack<char> bracketStack; // Create a stack to keep track of open brackets
-        
-        // Iterate through the characters in the input string
-        for (char c : s) {
-            if (c == '(' || c == '[' || c == '{') {
-                // If an open bracket is encountered, push it onto the stack
-                bracketStack.push(c);
-            } else {
-                // If a closing bracket is encountered, check if the stack is empty or if it matches the top element
-                if (bracketStack.empty() || !isMatching(bracketStack.top(), c)) {
-                    return false; // Mismatched or extra closing bracket
+
+        stack<char> st;
+        for (int i = 0;i<s.size();i++){
+            if (s[i]=='(' or s[i]=='[' or s[i] =='{'){
+                st.push(s[i]);
+            }
+            else{
+                char top = st.top();
+                if (!st.empty() and ((top=='(' and s[i]==')') or (top=='[' and s[i]==']') or (top=='{' and s[i] =='}'))){
+                    st.pop();
                 }
-                bracketStack.pop(); // Remove the matching open bracket
+                else{
+                    return false;
+                }
             }
         }
         
-        // Check if there are any open brackets left in the stack
-        return bracketStack.empty();
-    }
-    
-private:
-    bool isMatching(char open, char close) {
-        return (open == '(' && close == ')') || (open == '[' && close == ']') || (open == '{' && close == '}');
+        if (st.empty()) return true;
+        return false;
+        
     }
 };
