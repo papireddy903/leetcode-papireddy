fb267a231cbacec2aaad71372bb4d6912319e30b
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-05-09 00:00:38 +0530

    Time: 3 ms (97.38%) | Memory: 9.7 MB (19.02%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        
-        sort(s.begin(),s.end());
-        sort(t.begin(), t.end());
 
-        if (s==t){
-            return true;
+        if (s.size()!=t.size()) return false;
+        
+        unordered_map<char,int> mp;
+
+        for (int i = 0;i<s.size();i++){
+            mp[s[i]]++;
+            mp[t[i]]--;
         }
-        else{
-            return false;
+
+        for (auto it: mp){
+            if (it.second){
+                return false;
+            }
         }
+        return true;
+
+        
+        
         
     }
 };
