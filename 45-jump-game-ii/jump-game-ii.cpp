1325237a552dbcfd2723802f306fdbc000bd413e
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-05-09 12:59:59 +0530

    Time: 161 ms (11.90%) | Memory: 20.6 MB (5.76%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    private:
+    int helper(int ind, int end ,vector<int> &nums, vector<int> &dp){
+        if (ind >= end){
+            return 0;
+        }
+
+        if (dp[ind]!=-1){
+            return dp[ind];
+        }
+        int mini = 1e9+7;
+        int k = 0;
+        int steps = nums[ind];
+        for (int i=1;i<=steps;i++){
+            k = 1 + helper(ind+i,end,nums,dp);
+            mini = min(mini, k);
+        }
+
+        return dp[ind]=mini;
+
+
+
+    }
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1,-1);
+        return helper(0,n-1,nums,dp);
+        
+    }
+};
