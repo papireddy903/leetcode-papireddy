1e7f267bdef7bbf64be3e57c8b5d69710d15fda5
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-06-01 15:36:33 +0530

    Time: 6 ms (30.45%) | Memory: 11.2 MB (50.97%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int peak;
+        if (nums.size()==1) return 0;
+        if (nums[0] > nums[1]) return 0;
+        if (nums[nums.size()-1] > nums[nums.size()-2]) return nums.size()-1;
+        for (int i = 1;i<nums.size()-1;i++){
+            if (nums[i] > nums[i-1] and nums[i] > nums[i+1]){
+                peak = i;
+                break;
+            }
+
+        }
+        return peak;
+        
+    }
+};
