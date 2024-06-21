92a8c16e671acf377bd10be6d3a75823311c63d1
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-06-21 13:48:03 +0530

    Time: 5 ms (34.87%) | Memory: 7.4 MB (80.55%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        int l = 1, h = x;
+
+        while (l<=h){
+            int mid = l + (h-l)/2;
+
+            if (mid ==  x/mid){
+                return mid;
+            }
+            else if (mid > x/mid){
+                h = mid-1;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+
+        return h;
+    }
+};
