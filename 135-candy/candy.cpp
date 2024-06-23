16af5229e0015472ca69c928323c5a56fcd5562f
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-06-23 21:00:14 +0530

    Time: 15 ms (56.36%) | Memory: 19.3 MB (99.96%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
+
+        int sum = 1;
+        
+        int i = 1;
         int n = ratings.size();
-        vector<int> left(n,1);
-        // vector<int> right(n,1);
-        for (int i = 1;i<n;i++){
-            if (ratings[i] > ratings[i-1]){
-                left[i] = left[i-1]+1;
+
+        while (i < n){
+            if (ratings[i] == ratings[i-1]){
+                sum += 1;
+                i++;
+                continue;
             }
 
-        }
-
-        int right = 1;  // n-1 index
-        int cur = 1;   // n-2 index 
-        int sum = left[n-1];
-        for (int i = n-2;i>=0;i--){
-            if (ratings[i] > ratings[i+1]){
-                cur++;
+            int peak = 1;
+            while (i<n && ratings[i] > ratings[i-1]){
+                peak++;
+                sum+=peak;
+                i++;
                 
             }
-            else{
-                cur = 1;
-            }
-            sum += max(left[i], cur);
 
+            int down = 1;
+            while (i<n && ratings[i] < ratings[i-1]){
+                sum += down;
+                down++;
+                i++;
+            }
+
+            if (down > peak){
+                sum += down - peak;
+            }
         }
+
         return sum;
-   
+
+
         
     }
 };
