e3b8baf3eaa96c71cdf344c41b997a1152d5ae6f
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-06-29 11:59:26 +0530

    Time: 3 ms (13.64%) | Memory: 7.4 MB (9.76%) - LeetSync

---
+++ 292-nim-game/nim-game.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    private:
+    bool helper(int n, vector<int> &dp){
+        if (n<=3){
+            return true;
+        }
+
+        if (dp[n]!=-1){
+            return dp[n];
+        }
+
+        if (helper(n-1,dp) && helper(n-2,dp) && helper(n-3,dp)){
+            return dp[n] = false;
+        }
+        else{
+            return dp[n] = true;
+        }
+
+    }
+public:
+    bool canWinNim(int n) {
+
+        if (n > 1000) return n % 4 != 0;
+
+        vector<int> dp(n+1, -1);
+        
+        return helper(n,dp);
+
+
+        
+    }
+};
