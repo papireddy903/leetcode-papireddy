0ff4738e2c1e05e1d7dacd1313129d48ff408ab8
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-07-05 22:17:14 +0530

    Time: 11 ms (89.88%) | Memory: 17.1 MB (48.91%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -9,46 +9,33 @@
  * };
  */
 class Solution {
-    private:
-    ListNode* merge(ListNode* head1, ListNode* head2){
-        if (head1==NULL) return head2;
-        if (head2==NULL) return head1;
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<pair<int, ListNode*>, vector<pair<int,ListNode*>>, greater<pair<int,ListNode*>>> mheap;
+
+        for (auto it : lists){
+            if (it!=NULL)
+            mheap.push({it->val, it});
+        }
 
         ListNode* dummyHead = new ListNode(0);
         ListNode* temp = dummyHead;
 
-        while (head1!=NULL and head2!=NULL){
-            if (head1-> val <= head2->val){
-                temp->next= head1;
-                head1=head1->next;
+        while (!mheap.empty()){
+            auto mini = mheap.top(); mheap.pop();
+            
+            if (mini.second->next!=NULL){
+                mheap.push({mini.second->next->val, mini.second->next});
             }
-            else{
-                temp ->next = head2;
-                head2 = head2->next;
-            }
+            temp->next = mini.second;
             temp = temp->next;
-        }
-
-        if (head1 != NULL){
-            temp->next = head1;
-        }
-        else{
-            temp->next = head2;
         }
 
         return dummyHead->next;
 
-    }
-public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size()==0) return NULL;
-        ListNode* head = lists[0];
 
-        for (int i = 1;i<lists.size();i++){
-            head = merge(head, lists[i]);
-        }
 
-        return head;
+
         
     }
 };
