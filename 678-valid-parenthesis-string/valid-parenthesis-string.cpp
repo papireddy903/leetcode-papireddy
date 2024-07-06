81e8dcb554799de74906e3632f6628f8eaaed913
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-07-06 11:46:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (69.77%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,27 +1,25 @@
 class Solution {
-    private:
-    bool helper(int ind, string s, int count, vector<vector<int>> &dp){
-        int n = s.size();
-        if (ind < 0 or count < 0) return false;
-        if (ind == n) return count==0;
-
-        if (dp[ind][count]!=-1) return dp[ind][count];
-
-        if (s[ind]=='('){
-            return dp[ind][count]=helper(ind+1,s,count+1,dp);
-        }
-        if (s[ind]==')'){
-            return dp[ind][count]=helper(ind+1,s,count-1,dp);
-        }
-
-        return dp[ind][count]=helper(ind+1,s,count+1,dp) | helper(ind+1,s,count-1,dp) || helper(ind+1,s,count,dp);
-    }
 public:
     bool checkValidString(string s) {
-        int n = s.size();
-        vector<vector<int>> dp(n, vector<int> (n,-1));
-
-        return helper(0,s,0,dp);
+        int min = 0, max = 0;
+        
+        for (int i = 0;i<s.size();i++){
+            if (s[i] == '('){
+                min++; max++;
+            }
+            else if (s[i] == ')') {
+                // Closing parenthesis decreases both min and max
+                min--;
+                max--;
+            } else if (s[i] == '*') {
+                // Asterisk can be '(', ')' or empty
+                min--;  // Treat as ')'
+                max++;  // Treat as '('
+            }
+            if (min < 0) min = 0;
+            if (max < 0) return false;
+        }
+        return min==0;
         
     }
 };
