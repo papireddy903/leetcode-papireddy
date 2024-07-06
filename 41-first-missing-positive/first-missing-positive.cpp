8dc67740fe2e1bcf5c51a06ce6d382c322768594
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-07-06 15:32:42 +0530

    Time: 64 ms (33.91%) | Memory: 53.4 MB (51.89%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        for (auto it : nums){
-            mp[it]++;
+
+        int n = nums.size();
+
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
         }
 
-        int i = 1;
-        while (mp.find(i)!=mp.end()){
-            i++;
+        
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
         }
 
-        return i;
+        
+        return n + 1;
         
     }
 };
