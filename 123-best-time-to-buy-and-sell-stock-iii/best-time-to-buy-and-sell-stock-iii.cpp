fa153ac6f89ee40fc2c0602c5255703ecda201ac
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-07-08 21:29:02 +0530

    Time: 238 ms (60.59%) | Memory: 136.8 MB (61.53%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,16 +1,16 @@
 class Solution {
     private:
 
-    int helper(int ind, int buy, int cap, vector<vector<vector<int>>>&dp, vector<int> &prices){
-        if (ind==prices.size() or cap==0) return 0;
+    int helper(int ind, int transactions, vector<vector<int>>&dp, vector<int> &prices){
+        if (ind==prices.size() or transactions==4) return 0;
     
-        if (dp[ind][buy][cap]!=-1) return dp[ind][buy][cap];
+        if (dp[ind][transactions]!=-1) return dp[ind][transactions];
 
-        if (buy==1){
-            return dp[ind][buy][cap] = max(-prices[ind]+helper(ind+1, 0, cap, dp,prices), helper(ind+1,1,cap,dp,prices));
+        if (transactions%2==0){
+            return dp[ind][transactions] = max(-prices[ind]+helper(ind+1, transactions+1, dp,prices), helper(ind+1,transactions,dp,prices));
         }
         else{
-            return dp[ind][buy][cap] = max(prices[ind]+helper(ind+1,1,cap-1,dp,prices), helper(ind+1,0,cap,dp,prices));
+            return dp[ind][transactions] = max(prices[ind]+helper(ind+1,transactions+1,dp,prices), helper(ind+1,transactions,dp,prices));
         }
         return 0;
     }
@@ -18,29 +18,11 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        // vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int> (3,0)));
+        // vector<vector<vector<int>>> dp(n, vector<vector<int>> (2, vector<int> (3,-1)));
 
-        // return helper(0,1,2,dp, prices);
+        vector<vector<int>> dp(n, vector<int> (4, -1));
 
-        vector<vector<int>> cur(2, vector<int> (3,0));
-        vector<vector<int>> after(2, vector<int> (3,0));
-
-        for (int i = n-1;i>=0;i--){
-            for (int buy=0;buy<=1;buy++){
-                for (int cap=1;cap<=2;cap++){
-                    if (buy == 1){
-                        cur[buy][cap] = max(-prices[i]+after[0][cap], after[1][cap]);
-                    }
-                    else{
-                        cur[buy][cap] = max(prices[i] + after[1][cap-1],after[0][cap]);
-                    }
-                    
-                }
-            }
-            after = cur;
-
-        }
-        return cur[1][2];
+        return helper(0,0,dp, prices);
 
         
     }
