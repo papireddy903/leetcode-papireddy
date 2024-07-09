9797eaa27e630be679af20ec5816ee8f9309b626
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-07-09 16:27:44 +0530

    Time: 45 ms (36.69%) | Memory: 47.2 MB (27.20%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,30 +1,31 @@
 class Solution {
     private:
     int helper(int ind, vector<int> &coins, int amount, vector<vector<int>> &dp){
-        if (ind == 0){
-            if (amount % coins[ind] == 0){
-                return amount/coins[ind];
+        if (ind==0){
+            if (amount % coins[ind] == 0) {
+                return amount / coins[ind];
             }
             return 1e9;
+
         }
-        if (dp[ind][amount]!=-1){
-            return dp[ind][amount];
-        }
+
+        if (dp[ind][amount]!=-1) return dp[ind][amount];
+
         int take = 1e9;
-        int nottake = 0 + helper(ind-1, coins, amount, dp);
+        int nottake = 0 + helper(ind-1,coins,amount,dp);
         if (coins[ind] <= amount)
-        take=  1 + helper(ind, coins, amount-coins[ind],dp);
+        take = 1 + helper(ind,coins,amount-coins[ind],dp);
 
         return dp[ind][amount] = min(nottake, take);
     }
 public:
     int coinChange(vector<int>& coins, int amount) {
+
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int> (amount+1,-1));
-        int ans= helper(n-1, coins, amount, dp);
-        if (ans>=1e9){
-            return -1;
-        }
+        vector<vector<int>> dp(n, vector<int> (amount+1,-1));
+        int ans =  helper(coins.size()-1, coins, amount,dp);
+
+        if (ans>=1e9) return -1;
         return ans;
         
     }
