f3b473f35d6015f9d81357368096fd1d39de7ada
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-07-09 16:43:34 +0530

    Time: 51 ms (24.84%) | Memory: 46.9 MB (34.14%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -22,11 +22,39 @@ public:
     int coinChange(vector<int>& coins, int amount) {
 
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount+1,-1));
-        int ans =  helper(coins.size()-1, coins, amount,dp);
+        // vector<vector<int>> dp(n, vector<int> (amount+1,-1));
+        // int ans =  helper(coins.size()-1, coins, amount,dp);
 
-        if (ans>=1e9) return -1;
-        return ans;
+
+        // if (ans>=1e9) return -1;
+        // return ans;
+        vector<vector<int>> dp(n, vector<int> (amount+1,-1));
+
+        for (int i = 0;i<=amount;i++){
+            if (i%coins[0]==0){
+                dp[0][i] = i/coins[0];
+            }
+            else{
+                dp[0][i] = 1e9;
+            }
+        }
+
         
+        for (int i = 1;i<n;i++){
+            for (int j = 0;j<=amount;j++){
+                int notake = dp[i-1][j];
+                int take = 1e9;
+                if (coins[i] <= j){
+                    take = 1+dp[i][j - coins[i]];
+                }
+                dp[i][j] = min(take,notake);
+            }
+
+        }
+        if (dp[n-1][amount] >= 1e9){
+            return -1;
+        }
+
+        return dp[n-1][amount];
     }
 };
