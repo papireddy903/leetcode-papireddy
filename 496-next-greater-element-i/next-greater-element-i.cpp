aba1aa1748207c31b10ac35b55f43e546db8208f
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-07-10 14:32:40 +0530

    Time: 8 ms (31.96%) | Memory: 12.4 MB (6.65%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -2,25 +2,32 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
-        int m = nums2.size();
-
-        vector<int> ans;
-        for (int i = 0;i<n;i++){
-        int flag = 0;
-            for (int j = 0;j<m;j++){
-                if (nums2[j] == nums1[i]){
-                    for (int k = j+1;k<m;k++){
-                        if (nums2[k] > nums1[i]){
-                            flag = 1;
-                            ans.push_back(nums2[k]);
-                            break;
-                        }
-                    }
-                }
-            }
-            if (flag == 0) ans.push_back(-1);
+        unordered_map<int,int> mp;
+        stack<int> st;
+       for(auto it: nums2){
+        while(!st.empty() &&  st.top()<it){
+            mp[st.top()] = it;
+            st.pop();
         }
-        return ans;
+
+        st.push(it);
+
+       }
+       
+
+        // for (auto it : mp){
+        //     cout << it.first << " : " << it.second << endl;
+        // }
+        vector<int> res(n,-1);
+
+        for(int i=0;i<n;i++){
+            if (mp[nums1[i]]){
+                res[i]= mp[nums1[i]];
+                
+            }
+            
+        }
+        return res;
         
     }
 };
