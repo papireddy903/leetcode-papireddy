0dee0a8e34aa74dd4276adf310d66399ee1864f5
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-07-19 13:30:17 +0530

    Time: 3591 ms (25.32%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+
+SELECT contest_id, ROUND(COUNT(user_id) * 100.0 / (SELECT COUNT(DISTINCT user_id) FROM Users) ,2) AS percentage
+FROM Register
+GROUP BY contest_id 
+ORDER BY percentage DESC, contest_id ASC
+
+ 
