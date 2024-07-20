fcbe8db6b94914e99a4294a63cf6923edeaa39db
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-07-21 00:40:30 +0530

    Time: 3 ms (88.70%) | Memory: 10.9 MB (71.16%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,27 +1,33 @@
 class Solution {
-    private:
-    void helper(int ind, vector<int> &nums, int n, set<vector<int>> &s){
-
-        if (ind == n){
-            s.insert(nums);
+    void helper(vector<int>& nums, vector<int>& current, vector<vector<int>>& perms, vector<bool>& visited) {
+        if (current.size() == nums.size()) {
+            perms.push_back(current);
             return;
         }
         
-        for (int i = ind;i<n;i++){
-            swap(nums[ind], nums[i]);
-            helper(ind+1,nums,n,s);
-            swap(nums[ind], nums[i]);
+        for (int i = 0; i < nums.size(); i++) {
+            // Skip duplicates
+            if (visited[i] || (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1])) {
+                continue;
+            }
+            
+            visited[i] = true;
+            current.push_back(nums[i]);
+            helper(nums, current, perms, visited);
+            current.pop_back();
+            visited[i] = false;
         }
-        // return ans;
     }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        int n = nums.size();
-        set<vector<int>> s;
-        helper(0,nums,n,s);
-        vector<vector<int>> ans(s.begin(),s.end());
-        return ans;
+        vector<vector<int>> perms;
+        vector<int> current;
+        vector<bool> visited(nums.size(), false);
         
-
+        // Sort the numbers to handle duplicates
+        sort(nums.begin(), nums.end());
+        
+        helper(nums, current, perms, visited);
+        return perms;
     }
 };
