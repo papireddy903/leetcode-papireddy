b9977f6787e07ffe41ac52c891ce4e68a38c2ee3
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-07-21 00:48:07 +0530

    Time: 3 ms (88.70%) | Memory: 11 MB (71.16%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,33 +1,33 @@
 class Solution {
-    void helper(vector<int>& nums, vector<int>& current, vector<vector<int>>& perms, vector<bool>& visited) {
-        if (current.size() == nums.size()) {
-            perms.push_back(current);
+    private:
+    void dfs(vector<vector<int>> &perms, vector<int> &temp, unordered_map<int,int> &mp,vector<int> &nums){
+        if (temp.size()==nums.size()){
+            perms.push_back(temp);
             return;
         }
-        
-        for (int i = 0; i < nums.size(); i++) {
-            // Skip duplicates
-            if (visited[i] || (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1])) {
-                continue;
+
+        for (auto it : mp){
+            if (it.second > 0){
+                temp.push_back(it.first);
+                mp[it.first]--;
+                dfs(perms,temp,mp,nums);
+                temp.pop_back();
+                mp[it.first]++;
             }
-            
-            visited[i] = true;
-            current.push_back(nums[i]);
-            helper(nums, current, perms, visited);
-            current.pop_back();
-            visited[i] = false;
         }
     }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> perms;
-        vector<int> current;
-        vector<bool> visited(nums.size(), false);
-        
-        // Sort the numbers to handle duplicates
-        sort(nums.begin(), nums.end());
-        
-        helper(nums, current, perms, visited);
+        vector<int> temp;
+        unordered_map<int,int> mp;
+        for (auto it : nums){
+            mp[it]++;
+        }
+
+        dfs(perms,temp,mp, nums);
+
         return perms;
+        
     }
 };
