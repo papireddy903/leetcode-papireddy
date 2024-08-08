0eaffde92d463f6733929e195a3bb088c03c3d14
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-08-08 11:36:56 +0530

    Time: 24 ms (69.55%) | Memory: 38.9 MB (19.31%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -4,16 +4,13 @@ public:
         int count = 0;
         int maxi = 0;
         for (int i = 0;i<nums.size();i++){
-            if (i == nums.size()-1 or nums[i]==0){
-                if (nums[i] == 1) count++;
+            if (nums[i] == 1){
+                count++;
                 maxi = max(count, maxi);
-                cout << "maxi: " << maxi << endl;
-                count = 0;
             }
             else{
-                count++;
+                count=  0;
             }
-            // cout << "count : " << count << endl;
         }
         return maxi;
         
