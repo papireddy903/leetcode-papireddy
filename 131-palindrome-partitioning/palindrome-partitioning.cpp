653a9a8ff2876aa1c7c8ea4d5f8c2ad223ed0de1
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-08-18 10:13:50 +0530

    Time: 103 ms (42.55%) | Memory: 61.4 MB (37.04%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -5,32 +5,33 @@ class Solution {
             if (s[start] != s[end]){
                 return false;
             }
-            start++;
-            end--;
+            start++; end--;
         }
         return true;
     }
-    void helper(int start, vector<string> &path, vector<vector<string>> &ans, string s){
-        if (start == s.size()){
-            ans.push_back(path);
-            return;
+    void helper(int ind, string s, vector<vector<string>> &ans,vector<string> &ds){
+        if (ind == s.size()){
+            ans.push_back(ds);
+            return; 
         }
 
-        for (int i = start;i<s.size();i++){
-            if (isPalindrome(s,start,i)){
-                path.push_back(s.substr(start, i-start+1));
-                helper(i+1, path,ans,s);
-                path.pop_back();
-
+        for (int i = ind;i<s.size();i++){
+            if (isPalindrome(s, ind,i)){
+                ds.push_back(s.substr(ind, i-ind+1));
+                helper(i+1,s,ans,ds);
+                ds.pop_back();
             }
         }
+
+
+
+
     }
 public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
-        vector<string> path;
-        helper(0,path,ans,s);
+        vector<string> ds;
+        helper(0,s,ans,ds);
         return ans;
-        
     }
 };
