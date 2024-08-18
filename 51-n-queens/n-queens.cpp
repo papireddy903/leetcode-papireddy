def6534dbb33de65ddd62024b8fa5c1d4216eaeb
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-08-18 21:21:43 +0530

    Time: 7 ms (44.20%) | Memory: 9.5 MB (79.26%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,60 +1,67 @@
 class Solution {
     private:
-    bool isSafe(int row,int col,vector<vector<string>> &ans,vector<string>&board,int n){
-        int temprow = row;
-        int tempcol = col;
+    bool isSafe(int row, int col, vector<vector<string>> &ans, vector<string>&board, int n){
+        int store_r = row;
+        int store_c = col;
 
         while (row>=0 and col>=0){
-            if (board[row][col]=='Q'){
+            if (board[row][col] == 'Q'){
                 return false;
             }
             row--;
             col--;
         }
 
-        row = temprow;
-        col = tempcol;
+        row = store_r;
+        col = store_c;
 
-        while(col >=0){
+        while (col>=0){
             if (board[row][col] == 'Q') return false;
             col--;
         }
+        col = store_c;
 
-        col = tempcol;
-
-        while (row<n and col>=0){
+        while (row<n and col >= 0){
             if (board[row][col] == 'Q') return false;
             col--;
             row++;
         }
         return true;
+
+
+
+        
+
+
+
+
     }
-    void solve(int col,vector<vector<string>> &ans, vector<string> &board,int n){
-        if (col == n){
+    void helper(int col, vector<vector<string>> &ans, vector<string> &board, int n){
+        if (col ==n ){
             ans.push_back(board);
-            return ;
+            return;
         }
 
         for (int row = 0;row<n;row++){
-            if (isSafe(row,col,ans,board,n)){
+            if (isSafe(row, col, ans,board,n)){
                 board[row][col] = 'Q';
-                solve(col+1,ans,board,n);
+                helper(col+1, ans,board,n);
                 board[row][col] = '.';
             }
         }
-        
+
+
     }
 public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
         vector<string> board(n);
-        string s(n,'.');
+        string s(n, '.');
         for (int i = 0;i<n;i++){
             board[i] = s;
-
         }
 
-        solve(0,ans,board,n);
+        helper(0,ans,board,n);
         return ans;
         
     }
