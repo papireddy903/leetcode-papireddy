919d9df7f19b895d16695e46cd003dac86a3ebbb
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2024-09-06 20:17:40 +0530

    Time: 593 ms (9.62%) | Memory: 54.2 MB (5.86%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,40 +1,25 @@
 class Solution {
     private:
-    bool helper(int ind, vector<int> &nums, int end,vector<int> &dp){
-        if (ind >=end){
+    bool helper(int ind, int end, vector<int> &nums,vector<int> &dp){
+        if (ind>=end){
             return true;
         }
-
-        if (dp[ind]!=-1){
-            return dp[ind];
+        if (dp[ind]!=-1) return dp[ind];
+        for (int steps = 1;steps<=nums[ind];steps++){
+            if (helper(ind+steps,end, nums,dp)){
+                return dp[ind]=true;
+                
+            }
         }
-        int steps = nums[ind];
-        for (int i = 1;i<=steps;i++){
-            if (helper(ind+i, nums,end,dp))
-                return dp[ind] = true;
-        }
-
-        return dp[ind] = false;
-        
-
-
+        return dp[ind]=false;
     }
+
+    
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        // vector<int> dp(n+1,-1);
-        vector<bool> dp(n, false);
-        dp[n-1] = true;
-
-        for (int i = n-2;i>=0;i--){
-            for (int j = 1;j<=nums[i];j++){
-                if (dp[i+j] and i + j < n){
-                    dp[i] = true;
-                    break;
-                }
-            }
-        }
-        // return helper(0,nums,n-1,dp);
-        return dp[0];
+        vector<int> dp(n,-1);
+        return helper(0,n-1,nums,dp);
+        
     }
 };
